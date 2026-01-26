28c9f95384f2ec130ceb00ae1dc4a68aa1394d34
Kenneth DOSSOU <107636912+Kennethkenny30@users.noreply.github.com>
2026-01-26 18:46:20 +0100

    nest new backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { TrackingModule } from './tracking/tracking.module';
-import { StatsModule } from './stats/stats.module';
-import { SitesModule } from './sites/sites.module';
-import { NotificationsModule } from './notifications/notifications.module';
-import { AnalyticsModule } from './analytics/analytics.module';
-import { MonitoringModule } from './monitoring/monitoring.module';
-import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, TrackingModule, StatsModule, SitesModule, NotificationsModule, AnalyticsModule, MonitoringModule, ReportsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
